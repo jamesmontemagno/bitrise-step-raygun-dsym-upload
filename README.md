cf6044bda63e26a80bd7e29d5e2709c08ccac3f3
James Montemagno <james.montemagno@gmail.com>
2016-12-19 22:58:08 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Raygun dSYM Upload
+
+Use it to upload your dSYM to [Raygun](http://raygun.com).
+
+Can be run directly with the [bitrise CLI](https://github.com/bitrise-io/bitrise),
+just `git clone` this repository, `cd` into it's folder in your Terminal/Command Line
+and call `bitrise run test`.
+
+*Check the `bitrise.yml` file for required inputs which have to be
+added to your `.bitrise.secrets.yml` file!*
---
+++ step.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$raygun_api_key" ]; then
+	printf "\e[31mError: \e[0mraygun_api_key variable not set\n"
+	exit 1
+fi
+
+if [ -z "$dsym_path" ]; then
+	printf "\e[31mError: \e[0mdsym_path variable not set\n"
+	exit 1
+fi
+
+if [ -z "$app_id" ]; then
+	printf "\e[31mError: \e[0mapp_id variable not set\n"
+	exit 1
+fi
+
+shopt -s nocasematch
+
+zip_dsym_path=""
+if [[ -d "${dsym_path}" ]] ; then
+	printf "\e[34mZipping dSYM...\e[0m\n"
+	zip_dsym_path="${dsym_path}.zip"
+	zip -r "${zip_dsym_path}" "${dsym_path}"
+elif [[ -f "${dsym_path}" ]]; then
+	if [[ $dsym_path =~ \.zip$ ]] ; then
+		zip_dsym_path="${dsym_path}"
+	else
+		printf "\e[31mError: \e[0mUnsupported dSYM format\n"
+		exit 1
+	fi
+else
+	printf "\e[31mError: \e[0mFile not found at path ${dsym_path}\n"
+	exit 1
+fi
+
+printf "\e[34mUploading ${zip_dsym_path} to Raygun\e[0m\n"
+
+
+curl -w "Upload returned: %{http_code}\\n" -H "Host: app.raygun.com" -H "Authorization: Basic ${raygun_api_key} --form "DsymFile=@${dsym_path}" https://app.raygun.com/dashboard/${app_id}/settings/symbols
